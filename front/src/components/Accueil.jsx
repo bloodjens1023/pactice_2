b19659cba9001bfb82327628df157e5ae34f1e84
bloodjens1023 <bloodjens1023>
2024-03-15 23:37:21 +0100

    sds

--- front/src/components/Accueil.jsx
+++ front/src/components/Accueil.jsx
@@ -1,10 +1,8 @@
 import Headers from "./Headers";
-import jsonData from '../assets/img/img.json'; 
 function Accueil() {
     return ( 
         <>
             <Headers/>
-            <img src={jsonData.imageURL} alt="Image" />
         </>
      );
 }
