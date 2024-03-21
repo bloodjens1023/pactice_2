6adc84e0e85ff33c2caa506b5a27d27bb33791cf
bloodjens1023 <bloodjens1023>
2024-03-21 11:19:23 +0100

    ok

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,26 +1,23 @@
-import './App.css'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Connexion from './components/Connexion';
-import Inscription from './components/Inscription';
-import Accueil from './components/Accueil';
-import Paiement from './components/Paiement';
-import Loading from './components/Loading';
-import ValidPaiement from './components/ValidPaiement';
-import Produits from './components/Produit';
-import Profil from './components/Profil';
-import Paniers from './components/Paniers';
-import Cook from './components/Cook';
-import Dashboard from './components/Dashboard';
-import Vendre from './components/Vendre';
-import Evenement from './components/Evenement';
-import ValidVente from './components/ValidVente';
-
-
-
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Connexion from "./components/Connexion";
+import Inscription from "./components/Inscription";
+import Accueil from "./components/Accueil";
+import Paiement from "./components/Paiement";
+import Loading from "./components/Loading";
+import ValidPaiement from "./components/ValidPaiement";
+import Produits from "./components/Produit";
+import Profil from "./components/Profil";
+import Paniers from "./components/Paniers";
+import Cook from "./components/Cook";
+import Dashboard from "./components/Dashboard";
+import Vendre from "./components/Vendre";
+import Evenement from "./components/Evenement";
+import ValidVente from "./components/ValidVente";
 
 function App() {
   return (
-    <Routes  >
+    <Routes>
       <Route path="/" Component={Accueil} />
       <Route path="/Connexion" Component={Connexion} />
       <Route path="/Inscription" Component={Inscription} />
@@ -35,11 +32,8 @@ function App() {
       <Route path="/Dash" Component={Dashboard} />
       <Route path="/Vendre" Component={Vendre} />
       <Route path="/Evenement" Component={Evenement} />
-
-      
-
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
