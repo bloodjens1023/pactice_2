7a40abd5b788288f2bfffdd516d54b8c5698e8e4
bloodjens1023 <bloodjens1023>
2024-03-21 08:03:59 +0100

    reformat

--- front/src/components/CardProduit.jsx
+++ front/src/components/CardProduit.jsx
@@ -1,26 +1,27 @@
 import { useNavigate } from "react-router-dom";
-import React,{useState} from "react";
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import axios from "axios";
 //card produits
-function CardProduit({produits}) {
-    const navigate = useNavigate() 
-    const [show, setShow] = useState(false);
+function CardProduit({ produits }) {
+  const navigate = useNavigate();
+  const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-    function ajoutPanier(e,f) {
-        axios.post('http://localhost:8000/api/ajoutPanier',{e,f})
-        .then(res =>{
-            console.log(res.data)
-        })
-        .catch(res => console.error(res.data))
-    }
-    return ( <>
-
-<Modal show={show} onHide={handleClose}>
+  function ajoutPanier(e, f) {
+    axios
+      .post("http://localhost:8000/api/ajoutPanier", { e, f })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((res) => console.error(res.data));
+  }
+  return (
+    <>
+      <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Information</Modal.Title>
         </Modal.Header>
@@ -31,52 +32,101 @@ function CardProduit({produits}) {
           </Button>
         </Modal.Footer>
       </Modal>
-    
-    <div className="cards col-md-3">
-        <div className="image-container">
-            <img
-                style={{ height: "200px", width: "200px" }}
-                className="card-img-top"
-                src={produits.lien_image}
-                alt="First slide" />
 
-            <div className="price">{produits.prix} Tc</div>
+      <div className="cards col-md-3">
+        <div className="image-container">
+          <img
+            style={{ height: "200px", width: "200px" }}
+            className="card-img-top"
+            src={produits.lien_image}
+            alt="First slide"
+          />
+
+          <div className="price">{produits.prix} Tc</div>
         </div>
 
         <div className="content">
-            <div style={{ marginTop: "150px" }}></div>
-            <div className="brand">{produits.nom_produit}</div>
-            <div className="product-name">{produits.description}</div>
+          <div style={{ marginTop: "150px" }}></div>
+          <div className="brand">{produits.nom_produit}</div>
+          <div className="product-name">{produits.description}</div>
 
-            <div className="rating">
-                <svg viewBox="0 0 99.498 16.286" xmlns="http://www.w3.org/2000/svg" className="svg four-star-svg">
-                    <path fill="#fc0" transform="translate(-0.001 -1.047)" d="M9.357,1.558,11.282,5.45a.919.919,0,0,0,.692.5l4.3.624a.916.916,0,0,1,.509,1.564l-3.115,3.029a.916.916,0,0,0-.264.812l.735,4.278a.919.919,0,0,1-1.334.967l-3.85-2.02a.922.922,0,0,0-.855,0l-3.85,2.02a.919.919,0,0,1-1.334-.967l.735-4.278a.916.916,0,0,0-.264-.812L.279,8.14A.916.916,0,0,1,.789,6.576l4.3-.624a.919.919,0,0,0,.692-.5L7.71,1.558A.92.92,0,0,1,9.357,1.558Z" id="star-svgrepo-com"></path>
-                    <path fill="#fc0" transform="translate(20.607 -1.047)" d="M9.357,1.558,11.282,5.45a.919.919,0,0,0,.692.5l4.3.624a.916.916,0,0,1,.509,1.564l-3.115,3.029a.916.916,0,0,0-.264.812l.735,4.278a.919.919,0,0,1-1.334.967l-3.85-2.02a.922.922,0,0,0-.855,0l-3.85,2.02a.919.919,0,0,1-1.334-.967l.735-4.278a.916.916,0,0,0-.264-.812L.279,8.14A.916.916,0,0,1,.789,6.576l4.3-.624a.919.919,0,0,0,.692-.5L7.71,1.558A.92.92,0,0,1,9.357,1.558Z" data-name="star-svgrepo-com" id="star-svgrepo-com-2"></path>
-                    <path fill="#fc0" transform="translate(41.215 -1.047)" d="M9.357,1.558,11.282,5.45a.919.919,0,0,0,.692.5l4.3.624a.916.916,0,0,1,.509,1.564l-3.115,3.029a.916.916,0,0,0-.264.812l.735,4.278a.919.919,0,0,1-1.334.967l-3.85-2.02a.922.922,0,0,0-.855,0l-3.85,2.02a.919.919,0,0,1-1.334-.967l.735-4.278a.916.916,0,0,0-.264-.812L.279,8.14A.916.916,0,0,1,.789,6.576l4.3-.624a.919.919,0,0,0,.692-.5L7.71,1.558A.92.92,0,0,1,9.357,1.558Z" data-name="star-svgrepo-com" id="star-svgrepo-com-3"></path>
-                    <path fill="#fc0" transform="translate(61.823 -1.047)" d="M9.357,1.558,11.282,5.45a.919.919,0,0,0,.692.5l4.3.624a.916.916,0,0,1,.509,1.564l-3.115,3.029a.916.916,0,0,0-.264.812l.735,4.278a.919.919,0,0,1-1.334.967l-3.85-2.02a.922.922,0,0,0-.855,0l-3.85,2.02a.919.919,0,0,1-1.334-.967l.735-4.278a.916.916,0,0,0-.264-.812L.279,8.14A.916.916,0,0,1,.789,6.576l4.3-.624a.919.919,0,0,0,.692-.5L7.71,1.558A.92.92,0,0,1,9.357,1.558Z" data-name="star-svgrepo-com" id="star-svgrepo-com-4"></path>
-                    <path fill="#e9e9e9" transform="translate(82.431 -1.047)" d="M9.357,1.558,11.282,5.45a.919.919,0,0,0,.692.5l4.3.624a.916.916,0,0,1,.509,1.564l-3.115,3.029a.916.916,0,0,0-.264.812l.735,4.278a.919.919,0,0,1-1.334.967l-3.85-2.02a.922.922,0,0,0-.855,0l-3.85,2.02a.919.919,0,0,1-1.334-.967l.735-4.278a.916.916,0,0,0-.264-.812L.279,8.14A.916.916,0,0,1,.789,6.576l4.3-.624a.919.919,0,0,0,.692-.5L7.71,1.558A.92.92,0,0,1,9.357,1.558Z" data-name="star-svgrepo-com" id="star-svgrepo-com-5"></path>
-                </svg>
-                (29,062)
-            </div>
+          <div className="rating">
+            <svg
+              viewBox="0 0 99.498 16.286"
+              xmlns="http://www.w3.org/2000/svg"
+              className="svg four-star-svg"
+            >
+              <path
+                fill="#fc0"
+                transform="translate(-0.001 -1.047)"
+                d="M9.357,1.558,11.282,5.45a.919.919,0,0,0,.692.5l4.3.624a.916.916,0,0,1,.509,1.564l-3.115,3.029a.916.916,0,0,0-.264.812l.735,4.278a.919.919,0,0,1-1.334.967l-3.85-2.02a.922.922,0,0,0-.855,0l-3.85,2.02a.919.919,0,0,1-1.334-.967l.735-4.278a.916.916,0,0,0-.264-.812L.279,8.14A.916.916,0,0,1,.789,6.576l4.3-.624a.919.919,0,0,0,.692-.5L7.71,1.558A.92.92,0,0,1,9.357,1.558Z"
+                id="star-svgrepo-com"
+              ></path>
+              <path
+                fill="#fc0"
+                transform="translate(20.607 -1.047)"
+                d="M9.357,1.558,11.282,5.45a.919.919,0,0,0,.692.5l4.3.624a.916.916,0,0,1,.509,1.564l-3.115,3.029a.916.916,0,0,0-.264.812l.735,4.278a.919.919,0,0,1-1.334.967l-3.85-2.02a.922.922,0,0,0-.855,0l-3.85,2.02a.919.919,0,0,1-1.334-.967l.735-4.278a.916.916,0,0,0-.264-.812L.279,8.14A.916.916,0,0,1,.789,6.576l4.3-.624a.919.919,0,0,0,.692-.5L7.71,1.558A.92.92,0,0,1,9.357,1.558Z"
+                data-name="star-svgrepo-com"
+                id="star-svgrepo-com-2"
+              ></path>
+              <path
+                fill="#fc0"
+                transform="translate(41.215 -1.047)"
+                d="M9.357,1.558,11.282,5.45a.919.919,0,0,0,.692.5l4.3.624a.916.916,0,0,1,.509,1.564l-3.115,3.029a.916.916,0,0,0-.264.812l.735,4.278a.919.919,0,0,1-1.334.967l-3.85-2.02a.922.922,0,0,0-.855,0l-3.85,2.02a.919.919,0,0,1-1.334-.967l.735-4.278a.916.916,0,0,0-.264-.812L.279,8.14A.916.916,0,0,1,.789,6.576l4.3-.624a.919.919,0,0,0,.692-.5L7.71,1.558A.92.92,0,0,1,9.357,1.558Z"
+                data-name="star-svgrepo-com"
+                id="star-svgrepo-com-3"
+              ></path>
+              <path
+                fill="#fc0"
+                transform="translate(61.823 -1.047)"
+                d="M9.357,1.558,11.282,5.45a.919.919,0,0,0,.692.5l4.3.624a.916.916,0,0,1,.509,1.564l-3.115,3.029a.916.916,0,0,0-.264.812l.735,4.278a.919.919,0,0,1-1.334.967l-3.85-2.02a.922.922,0,0,0-.855,0l-3.85,2.02a.919.919,0,0,1-1.334-.967l.735-4.278a.916.916,0,0,0-.264-.812L.279,8.14A.916.916,0,0,1,.789,6.576l4.3-.624a.919.919,0,0,0,.692-.5L7.71,1.558A.92.92,0,0,1,9.357,1.558Z"
+                data-name="star-svgrepo-com"
+                id="star-svgrepo-com-4"
+              ></path>
+              <path
+                fill="#e9e9e9"
+                transform="translate(82.431 -1.047)"
+                d="M9.357,1.558,11.282,5.45a.919.919,0,0,0,.692.5l4.3.624a.916.916,0,0,1,.509,1.564l-3.115,3.029a.916.916,0,0,0-.264.812l.735,4.278a.919.919,0,0,1-1.334.967l-3.85-2.02a.922.922,0,0,0-.855,0l-3.85,2.02a.919.919,0,0,1-1.334-.967l.735-4.278a.916.916,0,0,0-.264-.812L.279,8.14A.916.916,0,0,1,.789,6.576l4.3-.624a.919.919,0,0,0,.692-.5L7.71,1.558A.92.92,0,0,1,9.357,1.558Z"
+                data-name="star-svgrepo-com"
+                id="star-svgrepo-com-5"
+              ></path>
+            </svg>
+            (29,062)
+          </div>
         </div>
 
         <div className="button-container">
-            <button className="buy-button button" onClick={() => {
-                ajoutPanier(produits.id_produit, produits.nom_produit);
-                navigate("/paniers");
-            } }>Acheter Direct</button>
-            <button className="cart-button button" onClick={() => {
-                handleShow()
-                ajoutPanier(produits.id_produit, produits.nom_produit);
-            } }>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
-                    <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0M9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0"/>
-                </svg>
-
-            </button>
+          <button
+            className="buy-button button"
+            onClick={() => {
+              ajoutPanier(produits.id_produit, produits.nom_produit);
+              navigate("/paniers");
+            }}
+          >
+            Acheter Direct
+          </button>
+          <button
+            className="cart-button button"
+            onClick={() => {
+              handleShow();
+              ajoutPanier(produits.id_produit, produits.nom_produit);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-cart-plus-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0M9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0" />
+            </svg>
+          </button>
         </div>
-    </div></>
-     );
+      </div>
+    </>
+  );
 }
 
 export default CardProduit;
